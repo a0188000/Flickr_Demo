df81a5971562d0cbd8504287afd0cb7f1d65fed6
weiting <a0188000@gmail.com>
2020-04-08 13:35:25 +0800

    Initial Commit

---
+++ Flickr_Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flickr_Demo
+//
+//  Created by EVERTRUST on 2020/4/8.
+//  Copyright © 2020 Shen Wei Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
